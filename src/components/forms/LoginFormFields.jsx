42449640a6c3583e504a676d9dc71407deecf0df
Alaa Elghamry <alaa.elghamry7@gmail.com>
2024-06-25 00:17:24 +0300

    chore: Refactor LoginForm component and related files

---
+++ src/components/forms/LoginFormFields.jsx
@@ -0,0 +1,34 @@
+import { TextField } from "@mui/material";
+import React from "react";
+
+export default function LoginFormFields({ formik }) {
+  return (
+    <>
+      <TextField
+        fullWidth
+        id="email"
+        name="email"
+        label="Email address"
+        value={formik.values.email}
+        onChange={formik.handleChange}
+        onBlur={formik.handleBlur}
+        error={formik.touched.email && Boolean(formik.errors.email)}
+        helperText={formik.touched.email && formik.errors.email}
+        margin="normal"
+      />
+      <TextField
+        fullWidth
+        id="password"
+        name="password"
+        label="Password"
+        type="password"
+        value={formik.values.password}
+        onChange={formik.handleChange}
+        onBlur={formik.handleBlur}
+        error={formik.touched.password && Boolean(formik.errors.password)}
+        helperText={formik.touched.password && formik.errors.password}
+        margin="normal"
+      />
+    </>
+  );
+}
---
+++ src/hooks/useLoginFormik.js
@@ -0,0 +1,42 @@
+// src/hooks/useLoginFormik.js
+import { useFormik } from "formik";
+import { useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { login } from "../store/slices/authSlice";
+import validationSchema from "../components/forms/validationSchema";
+
+const mockApiCall = (values) =>
+  new Promise((resolve) => {
+    setTimeout(() => {
+      resolve({ email: values.email });
+    }, 1000);
+  });
+
+const useLoginFormik = () => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const formik = useFormik({
+    initialValues: {
+      email: "",
+      password: "",
+    },
+    validationSchema,
+    onSubmit: async (values, { setSubmitting }) => {
+      try {
+        // Assuming mockApiCall is defined elsewhere and imported
+        const user = await mockApiCall(values);
+        dispatch(login(user));
+        navigate("/");
+      } catch (error) {
+        console.error("Error during login process:", error);
+      } finally {
+        setSubmitting(false);
+      }
+    },
+  });
+
+  return formik;
+};
+
+export default useLoginFormik;
